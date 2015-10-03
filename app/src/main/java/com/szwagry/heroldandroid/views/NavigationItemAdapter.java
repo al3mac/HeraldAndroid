6efbcec5e28f907508c83b0a81500b04e10365a6
almac <al3mac@gmail.com>
2015-10-03 10:26:15 +0200

    list

--- app/src/main/java/com/szwagry/heroldandroid/views/NavigationItemAdapter.java
+++ app/src/main/java/com/szwagry/heroldandroid/views/NavigationItemAdapter.java
@@ -1,7 +1,65 @@
 package com.szwagry.heroldandroid.views;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import org.androidannotations.annotations.AfterInject;
+import org.androidannotations.annotations.EBean;
+import org.androidannotations.annotations.RootContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ragnar on 10/3/15.
  */
-public class NavigationItemAdapter {
+
+@EBean
+public class NavigationItemAdapter extends BaseAdapter {
+
+    List<NavigationItem> navigationItems;
+
+    @RootContext
+    Context context;
+
+    @AfterInject
+    void initAdapter() {
+        navigationItems = new ArrayList<NavigationItem>();
+        navigationItems.add(new NavigationItem("1"));
+        navigationItems.add(new NavigationItem("2"));
+        navigationItems.add(new NavigationItem("3"));
+        navigationItems.add(new NavigationItem("4"));
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        NavigationItemView personItemView;
+        if (convertView == null) {
+            personItemView = NavigationItemView_.build(context);
+        } else {
+            personItemView = (NavigationItemView) convertView;
+        }
+
+        personItemView.bind(getItem(position));
+
+        return personItemView;
+    }
+
+    @Override
+    public int getCount() {
+        return navigationItems.size();
+    }
+
+    @Override
+    public NavigationItem getItem(int position) {
+        return navigationItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 }
