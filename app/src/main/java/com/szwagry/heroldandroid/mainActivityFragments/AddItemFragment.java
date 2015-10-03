f2f02041aad67baa306ad91afa1b287b80bc0311
almac <al3mac@gmail.com>
2015-10-03 15:52:20 +0200

    image with tag added

--- app/src/main/java/com/szwagry/heroldandroid/mainActivityFragments/AddItemFragment.java
+++ app/src/main/java/com/szwagry/heroldandroid/mainActivityFragments/AddItemFragment.java
@@ -1,5 +1,11 @@
 package com.szwagry.heroldandroid.mainActivityFragments;
 
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.UiThread;
 import android.support.v4.app.Fragment;
 import android.widget.ArrayAdapter;
@@ -11,10 +17,14 @@ import android.widget.Toast;
 import com.szwagry.heroldandroid.R;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -24,6 +34,7 @@ import java.util.List;
 @EFragment(R.layout.fragment_add_item)
 public class AddItemFragment extends Fragment {
 
+    ProgressDialog progressDialog;
     @ViewById
     Spinner spinnerItemTypes;
 
@@ -56,11 +67,13 @@ public class AddItemFragment extends Fragment {
             showToast("Name is empty!");
         } else{
             processRegistration(name, type, formatedData);
-            showToast("Created:" + name + " " + type + " " + formatedData);
         }
     }
 
-    private void processRegistration(String name, String type, String formatedData) {
+    @Background
+    void processRegistration(String name, String type, String formatedData) {
+
+        createAndSaveImageFromText("trolololo");
 
     }
 
@@ -83,4 +96,30 @@ public class AddItemFragment extends Fragment {
         }
         return false;
     }
+
+    public void createAndSaveImageFromText(String itemCode){
+        Bitmap src = BitmapFactory.decodeResource(getResources(), R.drawable.src); // the original file yourimage.jpg i added in resources
+        Bitmap dest;
+        dest = Bitmap.createBitmap(src.getWidth(), src.getHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas cs = new Canvas(dest);
+        Paint tPaint = new Paint();
+        tPaint.setTextSize(120);
+        tPaint.setColor(Color.BLACK);
+        tPaint.setStyle(Paint.Style.FILL);
+        cs.drawBitmap(src, 0f, 0f, null);
+        float height = tPaint.measureText("yY");
+        float width = tPaint.measureText(itemCode);
+        float x_coord = (src.getWidth() - width)/2;
+        cs.drawText(itemCode, x_coord, height+600f, tPaint); // 15f is to put space between top edge and the text, if you want to change it, you can
+        try {
+            dest.compress(Bitmap.CompressFormat.JPEG, 100, new FileOutputStream(new File("/sdcard/ImageAfterAddingText.jpg")));
+            // dest is Bitmap, if you want to preview the final image, you can display it on screen also before saving
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
 }
